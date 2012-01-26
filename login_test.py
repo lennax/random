43954385000f58a884cd858e18df5f7b763ecc4b
Lenna Peterson <arklenna@gmail.com>
2012-01-26 16:10:53 -0500

    Grabs header of page after attempted login.

--- login_test.py
+++ login_test.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 
+# User-specified strings are in all-caps
+
 import mechanize # for emulating browser
 
 try:
     # Start fake browser 
     br = mechanize.Browser()
-    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.16) Gecko/20111108 Iceweasel/3.5.16 (like Firefox/3.5.16)')]
     # Attempt to open url with fake browser
-    url = "http://www.example.com"
+    url = "http://www.EXAMPLE.com"
     try:
         br.open(url)
     # Exit if url fails to open
@@ -28,6 +29,8 @@ try:
     br.submit()
 
     # Read the html
+    header = br.response().info()
+    print header
     html = br.response().read()
     if "Error" in html:
         print "Login failed"
