4dc69b1e179550f829464b1937b96364f7aac91e
Lenna Peterson <arklenna@gmail.com>
2013-01-02 13:15:18 -0500

    Fixed egregious lack of function and poor flow.

--- url_fail.py
+++ url_fail.py
@@ -5,13 +5,15 @@ import urllib2
 url1 = "https://my.ipfw.edu"
 url2 = "http://fjjdjjdjdj.de"
 
-try: 
-    urllib2.urlopen(url1)
-    success1 = 1
-except urllib2.URLError:
-    print "URL '%s' failed to open" % url1
-if success1:
-    print "URL '%s' opened successfully" % url1
+
+def test_url(url1):
+    try:
+        urllib2.urlopen(url1)
+    except urllib2.URLError:
+        print "URL '%s' failed to open" % url1
+    else:
+        print "URL '%s' opened successfully" % url1
 
 
-
+test_url(url1)
+test_url(url2)
